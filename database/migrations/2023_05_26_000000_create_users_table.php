d45ad66764ec41dbac40d3754673c443d78688ab
HashimKhan923 <khanhash1994@gmail.com>
2023-07-11 13:15:12 +0500

    newtr

--- database/migrations/2023_05_26_000000_create_users_table.php
+++ database/migrations/2023_05_26_000000_create_users_table.php
@@ -13,10 +13,23 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('user_type')->default('customer');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('device_token')->nullable();
+            $table->string('avatar')->nullable();
+            $table->text('address')->nullable();
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('phone')->nullable();
+            $table->decimal('balance')->nullable();
+            $table->integer('banned')->default(0);
+            $table->string('referral_code')->nullable();
+            $table->boolean('is_active')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
